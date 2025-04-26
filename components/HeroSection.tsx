7c8ac9e79f7b4ff4925fee93a61994b1e2ae127f
Rayyan828 <cs22b2001@iiitdm.ac.in>
2025-04-26 16:25:18 +0530

    other files

--- components/HeroSection.tsx
+++
@@ -1,8 +0,0 @@
-export default function HeroSection() {
-    return (
-      <section className="text-center py-20 bg-gray-100">
-        <h2 className="text-4xl font-semibold mb-4">Welcome to Our Site</h2>
-        <p className="text-lg text-gray-600">We deliver great solutions for your needs.</p>
-      </section>
-    );
-  }
